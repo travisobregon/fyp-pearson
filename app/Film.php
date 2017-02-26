d4c3caa006e51759670437f3b47c28a4077b648b
Travis Obregon <travisobregon@Traviss-MacBook-Pro.local>
2017-02-26 19:46:51 +0000

    Add attribute casting

--- app/Film.php
+++ app/Film.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Film extends Model
 {
-    //
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'special_features' => 'array',
+    ];
 }
